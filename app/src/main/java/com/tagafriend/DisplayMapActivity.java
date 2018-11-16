961cfb83efcbec8b8403931fd5bbb6042ec0cf64
luisfernandezdelavara <lef16b@my.fsu.edu>
2018-11-15 20:24:08 -0500

    Login sign in works. MapsActivities work and pin user GPS.

--- app/src/main/java/com/tagafriend/DisplayMapActivity.java
+++ app/src/main/java/com/tagafriend/DisplayMapActivity.java
@@ -1,174 +1,122 @@
 package com.tagafriend;
 
+import android.app.Activity;
+import android.app.Service;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
+import com.google.android.gms.location.LocationListener;
 import android.location.LocationManager;
+
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.location.LocationListener;
-import android.Manifest;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import android.app.AlertDialog.Builder;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-
-
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DisplayMapActivity extends FragmentActivity implements OnMapReadyCallback {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+
+/**
+ * An activity that displays a Google map with a marker (pin) to indicate a particular location.
+ */
+
+public class DisplayMapActivity extends Activity {
+
+    MapView gMapView;
+    GoogleMap gMap = null;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    private static final int MY_PERMISSION_ACCESS_COARSE_LOCATION = 11;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_map);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_ACCESS_COARSE_LOCATION);
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            TextView latitudeTV = findViewById(R.id.tvLatitude);
+                            double latitude = location.getLatitude();
+                            String latitudeString = Double.toString(latitude);
+                            latitudeTV.setText(latitudeString);
+
+                            TextView longitudeTV = findViewById(R.id.tvLongitude);
+                            double longitude = location.getLongitude();
+                            String longitudeString = Double.toString(longitude);
+                            longitudeTV.setText(longitudeString);
+
+                            Intent newIntent = new Intent(getApplicationContext(), DisplayMapActivity2.class);
+                            Bundle b = new Bundle();
+                            b.putDouble("userLat", latitude);
+                            newIntent.putExtras(b);
+                            // Bundle extras = newIntent.getExtras();
+                            // extras.putDouble("userLatitude", latitude);
+                            // extras.putDouble("userLongtidude", longitude);
+                            // newIntent.putExtras(extras);
+                            startActivity(newIntent);
+                        }
+                    }
+                });
     }
 
-    // public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
-    //
-    // LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-    //
-    // private GoogleMap mMap;
-    //
-    // Criteria criteria = new Criteria();
-    //
-    // String provider = locationManager.getBestProvider(criteria, false);
-    //
-    // @Override
-    // protected void onCreate(Bundle savedInstanceState) {
-    //     super.onCreate(savedInstanceState);
-    //     setContentView(R.layout.activity_display_map);
-    //
-    //     // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-    //     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-    //             .findFragmentById(R.id.map);
-    //     mapFragment.getMapAsync(this);
-    // }
-    //
-    // /**
-    //  * Manipulates the map once available.
-    //  * This callback is triggered when the map is ready to be used.
-    //  * This is where we can add markers or lines, add listeners or move the camera. In this case,
-    //  * we just add a marker near Sydney, Australia.
-    //  * If Google Play services is not installed on the device, the user will be prompted to install
-    //  * it inside the SupportMapFragment. This method will only be triggered once the user has
-    //  * installed Google Play services and returned to the app.
-    //  */
-    // @Override
-    public void onMapReady(GoogleMap googleMap) {
-      //   mMap = googleMap;
-      //
-      //   Criteria criteria = new Criteria();
-      //
-      //   checkLocationPermission();
-      //
-      //   Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-      //
-      //   double lat = location.getLatitude();
-      //   double lng = location.getLongitude();
-      //
-      //   // Add a marker in Sydney and move the camera
-      //   LatLng sydney = new LatLng(lat,lng);
-      //   MarkerOptions marker = new MarkerOptions().position(sydney).title("Anuradha Rajashekar");
-      //   marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.userplaceholder));
-      //   mMap.addMarker(marker);
-      //   // mMap.addMarker(new MarkerOptions().position(sydney).title("Anuradha's location"));
-      //   mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-      //   mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
-      //
-      // /*  LatLng location2 = new LatLng(37.402276,-121.942161);
-      //   mMap.addMarker(new MarkerOptions().position(location2).title("Anuradha's new location"));
-      //  mMap.moveCamera(CameraUpdateFactory.newLatLng(location2));*/
-      //
-      //   //zoom into a particular position
-      //   CameraUpdate zoom = CameraUpdateFactory.zoomTo(12);
-      //   mMap.moveCamera(zoom);
-      //   mMap.animateCamera(zoom);
-    }
-    //
-    // public boolean checkLocationPermission() {
-    //     if (ContextCompat.checkSelfPermission(this,
-    //             Manifest.permission.ACCESS_FINE_LOCATION)
-    //             != PackageManager.PERMISSION_GRANTED) {
-    //
-    //         // Should we show an explanation?
-    //         if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-    //                 Manifest.permission.ACCESS_FINE_LOCATION)) {
-    //
-    //             // Show an explanation to the user *asynchronously* -- don't block
-    //             // this thread waiting for the user's response! After the user
-    //             // sees the explanation, try again to request the permission.
-    //             new AlertDialog.Builder(this)
-    //                     .setTitle(R.string.title_location_permission)
-    //                     .setMessage(R.string.text_location_permission)
-    //                     .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-    //                         @Override
-    //                         public void onClick(DialogInterface dialogInterface, int i) {
-    //                             //Prompt the user once explanation has been shown
-    //                             ActivityCompat.requestPermissions(DisplayMapActivity.this,
-    //                                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-    //                                     MY_PERMISSIONS_REQUEST_LOCATION);
-    //                         }
-    //                     })
-    //                     .create()
-    //                     .show();
-    //
-    //
-    //         } else {
-    //             // No explanation needed, we can request the permission.
-    //             ActivityCompat.requestPermissions(this,
-    //                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-    //                     MY_PERMISSIONS_REQUEST_LOCATION);
-    //         }
-    //         return false;
-    //     } else {
-    //         return true;
-    //     }
-    // }
-    //
-    // @Override
-    // public void onRequestPermissionsResult(int requestCode,
-    //                                        String permissions[], int[] grantResults) {
-    //     switch (requestCode) {
-    //         case MY_PERMISSIONS_REQUEST_LOCATION: {
-    //             // If request is cancelled, the result arrays are empty.
-    //             if (grantResults.length > 0
-    //                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-    //
-    //                 // permission was granted, yay! Do the
-    //                 // location-related task you need to do.
-    //                 if (ContextCompat.checkSelfPermission(this,
-    //                         Manifest.permission.ACCESS_FINE_LOCATION)
-    //                         == PackageManager.PERMISSION_GRANTED) {
-    //
-    //                     //Request location updates:
-    //                 }
-    //
-    //             } else {
-    //
-    //                 // permission denied, boo! Disable the
-    //                 // functionality that depends on this permission.
-    //
-    //             }
-    //             return;
-    //         }
-    //
-    //     }
-    // }
 }
+
--- app/src/main/java/com/tagafriend/Main2Activity.java
+++ app/src/main/java/com/tagafriend/Main2Activity.java
@@ -1,13 +1,48 @@
 package com.tagafriend;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private Button mapsActivityButton;
+
+    public FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        mapsActivityButton = findViewById(R.id.displayMapsButton);
+        client = LocationServices.getFusedLocationProviderClient(this);
+        mapsActivityButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                // Intent intent=new Intent(getApplicationContext(),SignInActivity.class);
+                // startActivity(intent);
+
+
+                            Intent intent=new Intent(getApplicationContext(),DisplayMapActivity.class);
+                            startActivity(intent);
+
+            }
+        });
     }
+
 }
